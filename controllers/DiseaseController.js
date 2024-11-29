97a34e9c6718bede65d2ca3f35fb40768efb39dc
Fathimatus Zahrotun Nisa <127000577+FathimatusZN@users.noreply.github.com>
2024-11-29 10:15:52 +0700

    Update DiseaseController.js

--- controllers/DiseaseController.js
+++ controllers/DiseaseController.js
@@ -1,44 +1,45 @@
 const { predict } = require("../utils/mlUtils");
 
 const classLabels = [
-  "Apple Scab",
-  "Black Rot on Apple",
-  "Black Rot on Grape",
-  "Cedar Apple Rust",
-  "Cherry Powdery Mildey",
-  "Corn Cercospora Leaf Spot Gray Leaf Spot",
-  "Corn Common Rust",
-  "Corn Healthy",
-  "Corn Northern Leaf Blight",
-  "Esca (Black Measles) on Grape",
-  "Grape Leaf Blight",
-  "Healthy Apple",
-  "Healthy Blueberry",
-  "Healthy Cherry",
-  "Healthy Grape",
-  "Healthy Peach",
-  "Healthy Pepper Bell",
-  "Healthy Potato",
-  "Healthy Raspberry",
-  "Healthy Soybean",
-  "Healthy Strawberry",
-  "Orange Huanglongbing (HLB) / Citrus Greening",
-  "Peach Bacterial Spot",
-  "Pepper Bell Bachterial Spot",
-  "Potato Early Blight",
-  "Potato Late Blight",
-  "Squash Powdery Mildew",
-  "Strawberrry Leaf Scorch",
-  "Tomato Bacterial Spot",
-  "Tomato Early Blight",
-  "Tomato Healthy",
-  "Tomato Late Blight",
-  "Tomato Leaf Mold",
-  "Tomato Mosaic Virus",
-  "Tomato Septoria Leaf Spot",
-  "Tomato Spider Mites Two Spotted",
-  "Tomato Target Spot",
-  "Tomato Yellow Leaf Curl Virus",
+  "Apple Apple scab", // 0
+  "Apple Black rot", // 1
+  "Apple Cedar apple rust", // 2
+  "Apple healthy", // 3
+  "Background without leaves", // 4
+  "Blueberry healthy", // 5
+  "Cherry Powdery mildew", // 6
+  "Cherry healthy", // 7
+  "Corn Cercospora leaf spot Gray leaf spot", // 8
+  "Corn Common rust", // 9
+  "Corn Northern Leaf Blight", // 10
+  "Corn healthy", // 11
+  "Grape Black rot", // 12
+  "Grape Esca (Black Measles)", // 13
+  "Grape Leaf blight (Isariopsis Leaf Spot)", // 14
+  "Grape healthy", // 15
+  "Orange Haunglongbing (Citrus greening)", // 16
+  "Peach Bacterial spot", // 17
+  "Peach healthy", // 18
+  "Pepper bell Bacterial spot", // 19
+  "Pepper bell healthy", // 20
+  "Potato Early blight", // 21
+  "Potato Late blight", // 22
+  "Potato healthy", // 23
+  "Raspberry healthy", // 24
+  "Soybean healthy", // 25
+  "Squash Powdery mildew", // 26
+  "Strawberry Leaf scorch", // 27
+  "Strawberry healthy", // 28
+  "Tomato Bacterial spot", // 29
+  "Tomato Early blight", // 30
+  "Tomato Late blight", // 31
+  "Tomato Leaf Mold", // 32
+  "Tomato Septoria leaf spot", // 33
+  "Tomato Spider mites Two-spotted spider mite", // 34
+  "Tomato Target Spot", // 35
+  "Tomato Tomato Yellow Leaf Curl Virus", // 36
+  "Tomato Tomato mosaic virus", // 37
+  "Tomato healthy", // 38
 ];
 
 let diseaseModel; // Model deteksi penyakit
