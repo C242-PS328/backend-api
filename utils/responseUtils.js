c53c3678d2b23f5bd1ba900f35dff7367a32714b
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2024-12-06 19:34:16 +0700

    refactor: response handling to controller

--- utils/responseUtils.js
+++ utils/responseUtils.js
@@ -1,9 +1,9 @@
-function successResponse(res, data, message = "Success") {
-  res.status(200).json({ status: "success", message, data });
+function successResponse(res, data, message = "Success", statusCode) {
+  res.status(statusCode).json({ status: "success", message, data });
 }
 
-function errorResponse(res, error, code = 500) {
-  res.status(code).json({ status: "error", message: error.message });
+function errorResponse(res, message, statusCode = 500) {
+  res.status(statusCode).json({ status: "error", message });
 }
 
 module.exports = { successResponse, errorResponse };
