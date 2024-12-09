e2af1eb507675109066d0ae132a67d71994f7370
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2024-12-09 09:07:04 +0700

    remove docker file and edit localhost

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://0.0.0.0:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
